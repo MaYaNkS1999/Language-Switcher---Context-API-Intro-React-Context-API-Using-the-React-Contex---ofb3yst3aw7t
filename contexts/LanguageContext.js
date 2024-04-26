9abbb562ec7c37084120df5e08be46059559aefc
Abhishek Chauhan <128348666+Abhishekbtech@users.noreply.github.com>
2024-04-26 22:45:55 +0530

    Update LanguageContext.js

---
+++ contexts/LanguageContext.js
@@ -0,0 +1,17 @@
+"use client"
+// contexts/LanguageContext.js
+import React, { createContext, useState } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('en');
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export default LanguageContext;
